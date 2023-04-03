93c80e0f5cf48c9bc2fa974869c04a4e98602ac3
CJ Hess <cjh147@lindenwood.edu>
2023-04-03 14:21:08 -0500

    Start of Homework 4
    Copied the C Lex Analyzer from book and started the python version.

---
+++ Homework 4/hw4-ans1.py
@@ -0,0 +1,38 @@
+# Constants
+INT_LIT = 10
+IDENT = 11
+ASSIGN_OP = 20
+ADD_OP = 21
+SUB_OP = 22
+MULT_OP = 23
+DIV_OP = 24
+LEFT_PAREN = 25
+RIGHT_PAREN = 26
+
+# Global Variables
+charClass = 0
+lexLen = 0
+token = 0
+nextToken = 0
+lexeme = ""
+nextChar = ''
+
+
+def addChar():
+
+
+def getChar():
+
+
+def getNonBlank():
+
+
+def lex():
+
+
+def main():
+    in_fp = open("front.in", "r")
+    
+
+if __name__ == "main":
+    main()
